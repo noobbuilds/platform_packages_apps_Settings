ae418b6b21cb434a99641401aef5bde70308481c
beanstown106 <nbenis106@gmail.com>
2016-03-29 22:37:29 -0400

    Build NoobSettings into settings [2/2]
    Change-Id: I0be173403ec4096e692c274bc42da2392257119
    
    *Edited for noobbuilds

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -159,4 +159,7 @@ public class Settings extends SettingsActivity {
     public static class DeviceSettings extends SettingsActivity { /* empty */ }
     public static class PersonalSettings extends SettingsActivity { /* empty */ }
     public static class SystemSettings extends SettingsActivity { /* empty */ }
+    
+    // Noob adds
+    public static class NoobSettingsActivity extends SettingsActivity { /* empty */ }
 }
